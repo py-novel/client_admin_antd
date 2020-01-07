0323a93670bc9f026d4cc8ab917924c02fd8ea30
dkvirus <3548310748@qq.com>
2020-01-07 15:49:51 +0800

    fix: tslint code

--- src/pages/gyyd/classify/index.tsx
+++ src/pages/gyyd/classify/index.tsx
@@ -10,19 +10,16 @@ export interface IClassify {
     desc: string;
 }
 
-interface IQueryFormProps extends FormComponentProps {}
-interface IEditModalProps extends FormComponentProps {}
-
 export default function () {
-    const [ list, setList ] = useState<Array<IClassify>>([])                    // 表格数据
-    const [ queryData, setQueryData ] = useState<{[key: string]: any}>({ _page: 1, _limit: 10 })        // 查询条件
-    const [ page, setPage ] = useState<PaginationConfig>()                      // 分页参数
-    const [ checkVisible, setCheckVisible ] = useState<boolean>(false)          // 查看模态框是否可见
-    const [ editVisible, setEditVisible ] = useState<boolean>(false)            // 新增/编辑模态框是否可见
-    const [ currentItem, setCurrentItem ] = useState<IClassify | null>()        // 表格当前操作行 
+    const [list, setList] = useState<Array<IClassify>>([])                    // 表格数据
+    const [queryData, setQueryData] = useState<{ [key: string]: any }>({ _page: 1, _limit: 10 })        // 查询条件
+    const [page, setPage] = useState<PaginationConfig>()                      // 分页参数
+    const [checkVisible, setCheckVisible] = useState<boolean>(false)          // 查看模态框是否可见
+    const [editVisible, setEditVisible] = useState<boolean>(false)            // 新增/编辑模态框是否可见
+    const [currentItem, setCurrentItem] = useState<IClassify | null>()        // 表格当前操作行 
 
     useEffect(() => {
-        async function fetchData () {
+        async function fetchData() {
             const result = await axios.get('/api/v1/classify', { params: queryData })
             const pagination = {
                 current: queryData._page,
@@ -39,14 +36,14 @@ export default function () {
     /**
      * 查询表单
      */
-    const QueryForm = ({ form }: IQueryFormProps) => {
+    const QueryForm = ({ form }: FormComponentProps) => {
         const { getFieldDecorator, getFieldsValue, resetFields } = form
 
         const search = () => {
             const data = getFieldsValue()
-            setQueryData({...queryData, ...data, _page: 1, _limit: 10 })
+            setQueryData({ ...queryData, ...data, _page: 1, _limit: 10 })
         }
-    
+
         const reset = () => {
             resetFields(Object.keys(getFieldsValue()))
             setQueryData({ _page: 1, _limit: 10 })
@@ -85,11 +82,11 @@ export default function () {
                 <Row type="flex" justify="space-between">
                     <Col>
                         <Button type="primary" icon="search" onClick={search}>查询</Button>
-                        <Button icon="reload" style={{ marginLeft: '16px' }}  onClick={reset}>重置</Button>
+                        <Button icon="reload" style={{ marginLeft: '16px' }} onClick={reset}>重置</Button>
                     </Col>
 
                     <Col>
-                        <Button type="primary" onClick={() => showEditModal()}>新增</Button>
+                        <Button type="primary" onClick={showEditModal}>新增</Button>
                     </Col>
                 </Row>
             </Form>
@@ -120,62 +117,65 @@ export default function () {
 
         const columns: ColumnProps<IClassify>[] = [
             {
-              title: '主键',
-              dataIndex: 'id',
-              align: 'center',
-              render: (text: any, record: any) => <a onClick={() => check(record)}>{text}</a>,
+                title: '主键',
+                dataIndex: 'id',
+                align: 'center',
+                render: (text: any, record: any) => <a onClick={() => check(record)}>{text}</a>,
             },
             {
-              title: '路径',
-              dataIndex: 'path',
+                title: '路径',
+                dataIndex: 'path',
             },
             {
-              title: '描述',
-              dataIndex: 'desc',
+                title: '描述',
+                dataIndex: 'desc',
             },
             {
                 title: '操作',
                 key: 'action',
                 width: 250,
                 render: (text: any, record: IClassify) => {
-                  return (
-                    <span>
-                      <Popconfirm title="确认删除?"
-                        okText="确认"
-                        cancelText="取消"
-                        onConfirm={() => remove(record)}>
-                        <Button type="link">删除</Button>
-                      </Popconfirm>
-                      <Divider type="vertical" />
-                      <Button type="link" onClick={() => showEditModal(record)}>
-                        编辑
+                    return (
+                        <span>
+                            <Popconfirm 
+                                title="确认删除?"
+                                okText="确认"
+                                cancelText="取消"
+                                onConfirm={() => remove(record)}>
+                                <Button type="link">删除</Button>
+                            </Popconfirm>
+                            <Divider type="vertical" />
+                            <Button type="link" onClick={() => showEditModal(record)}>
+                                编辑
                       </Button>
-                    </span>
-                  )
-                }, 
+                        </span>
+                    )
+                },
             }
         ]
 
         const pagination: PaginationConfig = {
             ...page,
             position: 'top',
-            onChange: (page, pageSize) => setQueryData({ ...queryData, _page: page, _limit: pageSize}),
+            onChange: (page, pageSize) => setQueryData({ ...queryData, _page: page, _limit: pageSize }),
         }
 
         return (
-            <Table dataSource={list}
+            <Table 
+                dataSource={list}
                 columns={columns}
                 size="middle"
                 bordered={true}
                 pagination={pagination}
-                rowKey="id" />
+                rowKey="id"
+            />
         )
     }
 
     /**
      * 新增/编辑模态框
      */
-    const EditModal = ({ form }: IEditModalProps) => {
+    const EditModal = ({ form }: FormComponentProps) => {
         const { getFieldDecorator, validateFields, getFieldsValue } = form
 
         const submit = () => {
@@ -208,12 +208,13 @@ export default function () {
         }
 
         return (
-            <Modal 
+            <Modal
                 visible={true}
                 maskClosable={false}
                 title={currentItem ? '编辑' : '新增'}
                 onOk={submit}
-                onCancel={close}>
+                onCancel={close}
+            >
                 <Form>
                     <Row>
                         <Col span={24}>
@@ -264,11 +265,12 @@ export default function () {
         if (!currentItem) return null
 
         return (
-            <Modal visible={true}
-                title="查看"
-                footer={null}
+            <Modal 
+                visible={true} 
+                title="查看" 
+                footer={null} 
                 onCancel={() => setCheckVisible(false)}
-                >
+            >
                 <Descriptions column={1} bordered={true}>
                     <Descriptions.Item label="主键">{currentItem.id}</Descriptions.Item>
                     <Descriptions.Item label="路径">{currentItem.path}</Descriptions.Item>
@@ -288,8 +290,8 @@ export default function () {
                 <TableComp />
             </Card>
 
-            { checkVisible && <CheckModal /> }
-            { editVisible && <AEditModal /> }
+            {checkVisible && <CheckModal />}
+            {editVisible && <AEditModal />}
         </>
     )
 }
